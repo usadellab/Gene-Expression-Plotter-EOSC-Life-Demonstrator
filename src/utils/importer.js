3869a6bfd2f1709f86cfd9c685782a7967bad7ac
wunderbarr <wunderbardan@gmail.com>
2021-01-15 15:20:21 +0100

    days as samples

--- src/utils/importer.js
+++ src/utils/importer.js
@@ -1,25 +1,23 @@
 import XLSX from 'xlsx';
+// const XLSX = require('xlsx');
+
 
 export async function importer(ab1, ab2) {
+// ( async () =>{
   try {
     const workbook_metadata = XLSX.read(new Uint8Array(ab1), {type:'array'});
     console.log(workbook_metadata.SheetNames);
     let worksheet_metadata = workbook_metadata.Sheets[workbook_metadata.SheetNames[0]];
-    // console.log(workbook_metadata);
-    // console.log(worksheet_metadata['A1'].v);
 
     const workbook_data = XLSX.read(new Uint8Array(ab2), {type:'array'});
     console.log(workbook_data.SheetNames);
     let worksheet_data = workbook_data.Sheets[workbook_data.SheetNames[0]];
-    // console.log(workbook_data);
-    // console.log(worksheet_data['A1'].v);
 
 
     // const workbook_data = XLSX.readFile('../assets/MOA2010-05_2_transcriptomics_microarrays_normalised.xlsx');
+    // let worksheet_data = workbook_data.Sheets[workbook_data.SheetNames[0]];
     
     let original_column_names = [];
-
-    // let worksheet_data = workbook_data.Sheets[workbook_data.SheetNames[0]];
     let table = new Object();
     let rows = new Object();
     for (let row = 0; row < 42035; ++row){
@@ -47,8 +45,8 @@ export async function importer(ab1, ab2) {
         rows[key] = value;
       }
     }
-    table.rows = rows;
-    // console.log(table.rows['MICRO.15041.C1']);
+    // table.rows = rows;
+    console.log(rows['MICRO.15041.C1']);
     // console.log(table['MICRO.15305.C1']);
     // console.log(table['cSTA24A5TH']);
     // console.log(table['BF_TUBSXXXX_0057H09_T3M.SCF']);
@@ -61,8 +59,8 @@ export async function importer(ab1, ab2) {
       let key_address = {c:2, r:row};
       let key_ref = XLSX.utils.encode_cell(key_address);
 
-      let group_address = {c:0, r:row};
-      let group_ref = XLSX.utils.encode_cell(group_address);
+      // let group_address = {c:0, r:row};
+      // let group_ref = XLSX.utils.encode_cell(group_address);
 
       let genotype_address = {c:5, r:row};
       let genotype_ref = XLSX.utils.encode_cell(genotype_address);
@@ -71,39 +69,93 @@ export async function importer(ab1, ab2) {
       let leaf_number_address = {c:8, r:row};
       let leaf_number_ref = XLSX.utils.encode_cell(leaf_number_address);
 
-      let plant_number_address = {c:7, r:row};
-      let plant_number_ref = XLSX.utils.encode_cell(plant_number_address);
+      // let plant_number_address = {c:7, r:row};
+      // let plant_number_ref = XLSX.utils.encode_cell(plant_number_address);
 
       let leaf_position_address = {c:9, r:row};
       let leaf_position_ref = XLSX.utils.encode_cell(leaf_position_address);
 
-      let date_address = {c:13, r:row};
-      let date_ref = XLSX.utils.encode_cell(date_address);
+      let date_treatment = {c:12, r:row};
+      let date_treatment_ref = XLSX.utils.encode_cell(date_treatment);
+
+      let date_observation = {c:13, r:row};
+      let date_observation_ref = XLSX.utils.encode_cell(date_observation);
 
       let replicate_address = {c:14, r:row};
       let replicate_ref = XLSX.utils.encode_cell(replicate_address);
 
       // console.log(worksheet_metadata[date_ref].w);
 
-      // group*sample*replicate
-      // Genotype_LeafNumber_LeafPosition -> group, Treatment -> sample. replicate (plant_number)->replicate
-      // let group = [worksheet_metadata[genotype_ref].v, 'Leaf'+worksheet_metadata[leaf_number_ref].v, worksheet_metadata[leaf_position_ref].v].join('_');
-      // let value = [group, worksheet_metadata[treatment_ref].v, 'Plant'+worksheet_metadata[plant_number_ref].v].join('*');
-      
       // group: genotype_date:observation_treatment; sample:leaf_position_leaf_number; replicate->replicate
       // worksheet_metadata[group_ref].v, 
-      let group = [worksheet_metadata[genotype_ref].v, worksheet_metadata[date_ref].w, worksheet_metadata[treatment_ref].v].join('_');
-      let sample = ['Leaf'+worksheet_metadata[leaf_number_ref].v, worksheet_metadata[leaf_position_ref].v].join('_');
-      let value = [group, sample, worksheet_metadata[replicate_ref].v].join('*');
+      //one replicate : group 44 (1+3), group 56 (1+2!+3), group 57 (1+2!+3) !wrong replicate number
+      //group 59 (1+2+3), group 68 (1+2+3)
+      //group 53: (1+2+3) inconsistent leaf number-> no new replicate1
+      let date_difference = worksheet_metadata[date_observation_ref].v-worksheet_metadata[date_treatment_ref].v;
+      let group = [worksheet_metadata[genotype_ref].v, worksheet_metadata[treatment_ref].v, 'Leaf'+worksheet_metadata[leaf_number_ref].v, worksheet_metadata[leaf_position_ref].v].join('_');
+      // let sample = [date_difference].join('_');
+      if (date_difference<10){
+        date_difference = '0'+date_difference;
+
+      }
+      let value = [group, date_difference, worksheet_metadata[replicate_ref].v].join('*');
       
 
       dict.set(worksheet_metadata[key_ref].v, value);
      
     } 
+    // let sliced_column_names = original_column_names.slice(2);
     let new_column_names = original_column_names.slice(2).map(name => dict.get(name));
-    table['header'] = new_column_names;
+    // table['header'] = new_column_names;
 
-    console.log([...table.header].sort());
+    
+    let sorted_header = [...new_column_names].sort();
+    // console.log(sorted_header);
+    let sorted_rows = new Object();
+
+    for (let row_id of Object.keys(rows)){
+      // console.log(row_id);
+      let sorted_row = [];
+      for (let col of sorted_header){
+        let index = new_column_names.indexOf(col);
+        sorted_row.push(rows[row_id][index]);
+        // let original_col_name = sliced_column_names[index];
+        // console.log(col+';'+index+';'+rows[row_id][index]+';'+original_col_name);
+      }
+      sorted_rows[row_id] = sorted_row;
+      // break;
+
+    }
+    // console.log(sorted_rows);
+    table.rows = sorted_rows;
+    table['header'] = sorted_header;
+
+    // let dropped_columns = [];
+    // // let group_sample = sorted_header.map(col => );
+    // // console.log(group_sample);
+    // let counts = {};
+    // sorted_header.forEach( (col) =>
+    // {
+    //   let gs = col.slice(0, col.length-5);
+    //   counts[gs] = counts[gs]? counts[gs]+1 :1;
+    // }
+    // );
+    // // console.log(counts);
+    // sorted_header.forEach( (col) =>
+    // {
+    //   let gs = col.slice(0, col.length-5);
+    //   if (counts[gs]<3){
+    //     dropped_columns.push(col);
+    //   }
+    // }
+    // );
+    // console.log(dropped_columns);
+
+   
+
+    console.log(sorted_header);
+
+
     return table;
 
   } catch (e){
@@ -111,3 +163,4 @@ export async function importer(ab1, ab2) {
   }
   
 }
+// )();
